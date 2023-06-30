3c6c4f44d1303e4307f82069c9225ce279cc4f87
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-06-29 19:10:29 -0500

    Cypress basic.cy.js tests corregido <p> tagline

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -11,8 +11,9 @@ const Brand = (props) => {
       <Link className="linkLogoYTaglineJuanjaramillo" href="/">
         <img
           className={styles.logojuanjaramillo}
-          alt="Logo juan jaramillo"
+          alt="Juan Jaramillo | Prompt Engineer · AI Expert · AI Consultant"
           src="https://generation-sessions.s3.amazonaws.com/7239e843ae9846c8aa9398eec9cc5734/img/logo-juan-jaramillo@2x.png"
+          title="Juan Jaramillo | Prompt Engineer · AI Expert · AI Consultant"
         />
         <p className={styles.taglinetienda}>
           Tienda en Línea de Productos y Servicios de IA.
